2e8cd05e7e0c844f78d805ca52e9a1ab973d478f
Oded <raoded@campus.technion.ac.il>
2018-06-14 19:40:54 +0300

    after tests

--- intercept.c
+++ intercept.c
@@ -1,15 +1,15 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
-// TODO: add more #include statements as necessary
+#include <asm/unistd.h>
 #include <linux/utsname.h>
 #include <linux/sched.h>
+#include <stdio.h>
 
 MODULE_LICENSE("GPL");
 
 #define MAX_NAME_LENGTH 16
-#define DEFAULT_SCAN_RANGE 100
+#define DEFAULT_SCAN_RANGE 200
 
-// TODO: add command-line arguments
 void** sys_call_table = NULL;
 int scan_range = DEFAULT_SCAN_RANGE;
 char* program_name = NULL;
@@ -36,26 +36,26 @@ void restore_params_to_default(){
 	scan_range = DEFAULT_SCAN_RANGE;
 }
 
-// TODO: import original syscall and write new syscall
 asmlinkage long (*orig_sys_kill)(pid_t,int);
 
 asmlinkage long my_sys_kill(pid_t pid, int sig){
 	//find the task
 	task_t *p = find_task_by_pid(pid);
+	
 	//if the command is to kill the program named program_name, return an error
-	if(sig == SIGKILL && is_prog_name(p->comm)){
+	if(sig == SIGKILL && !strcmp(program_name,p->comm)){
 		return -EPERM;
 	}
+	
 	//otherwise, use the original syscall
 	return orig_sys_kill(pid, sig);
 }
 
 void find_sys_call_table(int scan_range) {
-   // TODO: complete the function
    int i;
    void** current_pointer = &system_utsname;
    for(i=0; i<scan_range; i++ && current_pointer++){
-	   if(current_pointer[__NR_read] == sys_read){
+	   if(current_pointer[__NR_read] == (void*)&sys_read){
 		   sys_call_table = current_pointer;
 		   return;
 	   }
@@ -63,25 +63,31 @@ void find_sys_call_table(int scan_range) {
 }
 
 int init_module(void) {
-   // TODO: complete the function
    //if the program name was not inputted or the input range is invalid
    if(program_name == NULL || scan_range <= 0){
 	   restore_params_to_default();
 	   return 0;
    }
    
-   //try and find the sys_call_table
-   find_sys_call_table(scan_range);
+   while(sys_call_table==NULL){
+		//try and find the sys_call_table
+		find_sys_call_table(scan_range);
+		scan_range*=2;
+   }
+   
    //if we did not find it, reset the parameters and return
    if(sys_call_table == NULL){
 	   restore_params_to_default();
 	   return 0;
-   }
-   
+   }  
+  
    //save the old syscall
-   orig_sys_kill = sys_call_table[__NR_kill];
+   orig_sys_kill = (void*)sys_call_table[__NR_kill];	
+	
    //replace it with the new syscall
    sys_call_table[__NR_kill] = my_sys_kill;
+   
+   return 0;
 }
 
 void cleanup_module(void) {
@@ -92,6 +98,7 @@ void cleanup_module(void) {
    
    //restore the old syscall
    sys_call_table[__NR_kill] = orig_sys_kill;
+   
    //return parameters to default
    restore_params_to_default();
 }
